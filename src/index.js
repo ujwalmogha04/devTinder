99d78fa76b8e09c0349f8b2b64b54c87f51bb9d8
ujwal <ujwalmogha2004@gmail.com>
2025-06-09 16:46:35 +0530

    Added user routes

--- src/index.js
+++ src/index.js
@@ -2,24 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const {userModel} = require("./db");
+const userRouter = require("./routes/userRoutes");
 const app = express();
 
+app.use(express.json());
+
+app.use("/api/v1/user" , userRouter)
 
 async function main() {
     try {
 
         await mongoose.connect(process.env.MONGO_URL);
-
-        const existing = await userModel.findOne({ username: "test1" });
-        if (!existing) {
-            const testUser = await userModel.create({
-                username: "test1",
-                password: "12345"
-            });
-            console.log(" Test user created");
-        } else {
-            console.log("Test user already exists.");
-        }
+        
         app.listen(3000, () => {
             console.log("Server listening on port 3000")
         })
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,122 @@
+const { Router } = require("express");
+const { userModel } = require("../db");
+const bcrypt = require("bcryptjs")
+const userRouter = Router();
+
+userRouter.post("/signup", async (req, res) => {
+    const { username, name, password } = req.body;
+    try {
+        const existingUser = await userModel.findOne({ username });
+
+        if (existingUser) {
+            return res.status(409).json({
+                message: "user already exists"
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await userModel.create({
+            username,
+            name,
+            password: hashedPassword
+        })
+
+        return res.status(201).json({
+            message: "user created succesfully"
+        })
+
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+})
+
+userRouter.post("/signin", async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const existingUser = await userModel.findOne({ username });
+
+        if (!existingUser) {
+            return res.status(409).json({
+                message: "user doesn't exists signup first"
+            })
+        }
+
+        const matchedPassword = await bcrypt.compare(password, existingUser.password)
+
+        if (!matchedPassword) {
+            return res.status(401).json({
+                message: "Invalid credentials"
+            });
+        }
+
+        return res.status(201).json({
+            message: "signin sucessfully"
+        })
+
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+})
+
+userRouter.patch("/update", async (req, res) => {
+    const { name, password, username } = req.body;
+    try {
+        const existingUser = await userModel.findOne({ username });
+
+        if (!existingUser) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        const updateData = {};
+
+        if (name) updateData.name = name;
+        if (password) {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            updateData.password = hashedPassword;
+        }
+
+        await userModel.updateOne({ username }, { $set: updateData });
+
+        return res.status(200).json({
+            message: "User updated successfully"
+        })
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+})
+
+userRouter.delete("/delete", async (req, res) => {
+    const { username } = req.body;
+
+    try {
+        const deletedUser = await userModel.findOneAndDelete({ username });
+
+        if (!deletedUser) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        return res.status(200).json({
+            message: "User deleted successfully"
+        });
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+})
+
+module.exports = userRouter;
